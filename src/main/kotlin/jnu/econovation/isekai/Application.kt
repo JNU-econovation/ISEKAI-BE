ccf1a57a36df791cdcce0411274c5b5a001dbc71
rdme0 <71927381+rdme0@users.noreply.github.com>
2025-09-22 16:43:04 +0900

    :sparkles: feat: gemini 설정

--- src/main/kotlin/jnu/econovation/isekai/Application.kt
+++ src/main/kotlin/jnu/econovation/isekai/Application.kt
@@ -1,13 +1,18 @@
 package jnu.econovation.isekai
 
 import jnu.econovation.isekai.common.security.config.AllowedOriginsProperties
+import jnu.econovation.isekai.gemini.config.GeminiConfig
 import jnu.econovation.isekai.rtzr.config.RtzrConfig
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-@EnableConfigurationProperties(RtzrConfig::class, AllowedOriginsProperties::class)
+@EnableConfigurationProperties(
+    RtzrConfig::class,
+    AllowedOriginsProperties::class,
+    GeminiConfig::class
+)
 class Application
 
 fun main(args: Array<String>) {
