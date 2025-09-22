8b9863d3aea3557d3f7a59cd754830683a2396ed
rdme0 <71927381+rdme0@users.noreply.github.com>
2025-09-22 14:03:04 +0900

    :sparkles: feat: stt 구현

--- src/main/kotlin/jnu/econovation/isekai/Application.kt
+++ src/main/kotlin/jnu/econovation/isekai/Application.kt
@@ -1,9 +1,13 @@
 package jnu.econovation.isekai
 
+import jnu.econovation.isekai.common.security.config.AllowedOriginsProperties
+import jnu.econovation.isekai.rtzr.config.RtzrConfig
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties(RtzrConfig::class, AllowedOriginsProperties::class)
 class Application
 
 fun main(args: Array<String>) {
