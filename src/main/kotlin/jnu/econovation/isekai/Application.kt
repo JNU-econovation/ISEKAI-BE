0788cac5b7fe1858205839b91209d5714802bd7e
rdme0 <71927381+rdme0@users.noreply.github.com>
2025-09-22 13:15:52 +0900

    :sparkles: feat: initial commit

---
+++ src/main/kotlin/jnu/econovation/isekai/Application.kt
@@ -0,0 +1,11 @@
+package jnu.econovation.isekai
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IsekAiApplication
+
+fun main(args: Array<String>) {
+    runApplication<IsekAiApplication>(*args)
+}
---
+++ src/test/kotlin/jnu/econovation/isekai/IsekAiApplicationTests.kt
@@ -0,0 +1,13 @@
+package jnu.econovation.isekai
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IsekAiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
