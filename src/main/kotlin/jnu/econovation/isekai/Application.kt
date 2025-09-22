d2819f4fdebecedc6fa7b951893f410023af10b8
rdme0 <71927381+rdme0@users.noreply.github.com>
2025-09-22 13:25:54 +0900

    :truck: rename: 클래스 이름 변경

--- src/main/kotlin/jnu/econovation/isekai/Application.kt
+++ src/main/kotlin/jnu/econovation/isekai/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class IsekAiApplication
+class Application
 
 fun main(args: Array<String>) {
-    runApplication<IsekAiApplication>(*args)
+    runApplication<Application>(*args)
 }
