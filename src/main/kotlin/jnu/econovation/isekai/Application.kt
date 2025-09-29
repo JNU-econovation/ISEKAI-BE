fbbc87e6bd21cc9faa92c261e7fbaafa8416cfdc
rdme0 <we6610@gmail.com>
2025-09-30 00:21:50 +0900

    :fire: remove: RTZR STT 제거
    gemini live stt로 대체

--- src/main/kotlin/jnu/econovation/isekai/Application.kt
+++ src/main/kotlin/jnu/econovation/isekai/Application.kt
@@ -2,14 +2,12 @@ package jnu.econovation.isekai
 
 import jnu.econovation.isekai.common.security.config.AllowedOriginsProperties
 import jnu.econovation.isekai.gemini.config.GeminiConfig
-import jnu.econovation.isekai.rtzr.config.RtzrConfig
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
 @EnableConfigurationProperties(
-    RtzrConfig::class,
     AllowedOriginsProperties::class,
     GeminiConfig::class
 )
