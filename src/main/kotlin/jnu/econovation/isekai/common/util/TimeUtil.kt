34daaec59f1514687dbf0979e22f6c3386d47ab0
rdme0 <we6610@gmail.com>
2025-09-30 21:09:35 +0900

    :sparkles: feat: Instant -> Zoned Time 유틸 구현

---
+++ src/main/kotlin/jnu/econovation/isekai/common/util/TimeUtil.kt
@@ -0,0 +1,22 @@
+package jnu.econovation.isekai.common.util
+
+
+import java.time.Instant
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+
+object TimeUtil {
+    private val SEOUL_ZONE_ID: ZoneId = ZoneId.of("Asia/Seoul")
+    private val SIMPLE_DATE_FORMATTER: DateTimeFormatter =
+        DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+
+    fun convertInstantToZonedDateTime(
+        instant: Instant, zoneId: ZoneId = SEOUL_ZONE_ID
+    ): ZonedDateTime = ZonedDateTime.ofInstant(instant, zoneId)
+
+
+    fun formatToSimpleString(dateTime: ZonedDateTime): String {
+        return dateTime.format(SIMPLE_DATE_FORMATTER)
+    }
+}
