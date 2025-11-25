597571760717aecfc9d4b45bd407852c37fd0de9
rdme0 <71927381+rdme0@users.noreply.github.com>
2025-11-25 21:41:51 +0900

    :fire: remove: 이벤트 기반 아키텍처 삭제

--- src/main/kotlin/jnu/econovation/isekai/session/dto/internal/GeminiResponseDTO.kt
+++
@@ -1,9 +0,0 @@
-package jnu.econovation.isekai.session.dto.internal
-
-import jnu.econovation.isekai.gemini.dto.client.response.GeminiLiveResponse
-import jnu.econovation.isekai.session.event.EventContent
-
-data class GeminiResponseDTO(
-    val response: GeminiLiveResponse,
-    val personaId: Long
-) : EventContent
