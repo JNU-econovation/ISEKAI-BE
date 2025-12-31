cace43f3a5cc6447c0ac2806def2c406f0b45289
rdme0 <we6610@gmail.com>
2026-01-01 01:45:09 +0900

    :sparkles: feat: member role, oauth provider 필드 추가

--- src/main/java/jnu/econovation/isekai/member/entity/Member.java
+++ src/main/java/jnu/econovation/isekai/member/entity/Member.java
@@ -4,11 +4,15 @@ import jakarta.persistence.AttributeOverride;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jnu.econovation.isekai.common.entity.BaseEntity;
 import jnu.econovation.isekai.member.vo.Email;
+import jnu.econovation.isekai.member.vo.Oauth2Provider;
+import jnu.econovation.isekai.member.vo.Role;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -26,8 +30,18 @@ public class Member extends BaseEntity {
     @AttributeOverride(name = "value", column = @Column(name = "email", nullable = false, unique = true))
     private Email email;
 
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Oauth2Provider provider;
+
     @Builder
-    Member(Email email) {
+    Member(Oauth2Provider provider, Email email) {
+        this.provider = provider;
         this.email = email;
+        this.role = Role.USER;
     }
 }
--- src/main/kotlin/jnu/econovation/isekai/member/constant/MemberConstants.kt
+++ src/main/kotlin/jnu/econovation/isekai/member/constant/MemberConstants.kt
@@ -1,7 +1,9 @@
 package jnu.econovation.isekai.member.constant
 
 import jnu.econovation.isekai.member.vo.Email
+import jnu.econovation.isekai.member.vo.Oauth2Provider
 
 object MemberConstants {
     val MASTER_EMAIL = Email("admin@isekai.com")
+    val MASTER_PROVIDER = Oauth2Provider.KAKAO
 }
