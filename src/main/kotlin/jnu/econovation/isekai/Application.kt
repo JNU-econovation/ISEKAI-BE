c4762898c830556dc27bdbdc965e6e5c232e9afa
rdme0 <we6610@gmail.com>
2026-01-01 01:48:07 +0900

    :sparkles: feat: add CloudStorageProperties and UriSecurityConfig to Application configuration

--- src/main/kotlin/jnu/econovation/isekai/Application.kt
+++ src/main/kotlin/jnu/econovation/isekai/Application.kt
@@ -1,10 +1,11 @@
 package jnu.econovation.isekai
 
 import jnu.econovation.isekai.aiServer.config.AiServerConfig
-import jnu.econovation.isekai.common.security.config.AllowedOriginsProperties
+import jnu.econovation.isekai.common.security.config.UriSecurityConfig
 import jnu.econovation.isekai.gemini.config.GeminiConfig
 import jnu.econovation.isekai.prompt.config.PromptConfig
 import jnu.econovation.isekai.rtzr.config.RtzrConfig
+import jnu.econovation.isekai.storage.config.CloudStorageProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
@@ -13,11 +14,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableConfigurationProperties(
-    AllowedOriginsProperties::class,
+    UriSecurityConfig::class,
     GeminiConfig::class,
     PromptConfig::class,
     RtzrConfig::class,
-    AiServerConfig::class
+    AiServerConfig::class,
+    CloudStorageProperties::class
 )
 class Application
 
