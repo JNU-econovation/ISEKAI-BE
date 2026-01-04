cf9a7eccec65d8b4176936323c3ba63f19eb820c
rdme0 <we6610@gmail.com>
2026-01-04 23:51:37 +0900

    :sparkles: feat: gemini 이미지 생성 기능 구현

--- src/main/kotlin/jnu/econovation/isekai/gemini/client/GeminiClient.kt
+++ src/main/kotlin/jnu/econovation/isekai/gemini/client/GeminiClient.kt
@@ -7,7 +7,10 @@ import jnu.econovation.isekai.common.exception.server.InternalServerException
 import jnu.econovation.isekai.gemini.config.GeminiConfig
 import jnu.econovation.isekai.gemini.constant.enums.GeminiModel
 import kotlinx.coroutines.future.await
+import mu.KotlinLogging
 import org.springframework.stereotype.Component
+import software.amazon.awssdk.core.internal.waiters.ResponseOrException.response
+import kotlin.jvm.optionals.getOrNull
 
 
 @Component
@@ -15,6 +18,10 @@ class GeminiClient(
     private val mapper: ObjectMapper,
     config: GeminiConfig
 ) {
+    companion object {
+        private val logger = KotlinLogging.logger {}
+    }
+
     private val client = Client.builder().apiKey(config.apiKey).build()
 
     suspend fun getEmbedding(
@@ -54,6 +61,37 @@ class GeminiClient(
             ?: throw InternalServerException(IllegalStateException("Gemini 응답이 비었습니다."))
 
         return response
+    }
+
+    fun getImageResponse(
+        prompt: String,
+        model: GeminiModel = GeminiModel.NANO_BANANA
+    ): ByteArray {
+        val userContent = Content.fromParts(Part.fromText(prompt))
+
+        val response = client.models.generateContent(
+            model.toString(),
+            userContent,
+            null
+        )
+
+        logger.debug { "image response -> $response" }
+        val parts = response.candidates()
+            .get().first()?.content()?.get()?.parts()?.getOrNull()
+
+        if (parts == null)
+            throw InternalServerException(IllegalStateException("Gemini 응답이 비었습니다."))
+
+        for (part in parts) {
+            if (part.text().isPresent) {
+                logger.debug { "part.text -> ${part.text()}" }
+                continue
+            }
+            if (part.inlineData().isPresent)
+                return part.inlineData().get().data().get()
+        }
+
+        throw InternalServerException(IllegalStateException("Gemini 응답이 비었습니다."))
     }
 
     private fun buildConfig(
