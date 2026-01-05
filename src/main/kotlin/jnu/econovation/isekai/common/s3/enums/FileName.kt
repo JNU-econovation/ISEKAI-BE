fa1d37fd3d187112a942a99b61e917ce7eb517f1
rdme0 <we6610@gmail.com>
2026-01-05 17:59:26 +0900

    :sparkles: feat: FileName에 새로운 파일명 타입 추가

--- src/main/kotlin/jnu/econovation/isekai/common/s3/enums/FileName.kt
+++ src/main/kotlin/jnu/econovation/isekai/common/s3/enums/FileName.kt
@@ -2,7 +2,9 @@ package jnu.econovation.isekai.common.s3.enums
 
 enum class FileName(val text: String, private val extension: FileExtension) {
     LIVE2D_MODEL_FILE_NAME("live2d_model", extension = FileExtension.ZIP),
-    BACKGROUND_IMAGE_FILE_NAME("background_image", extension = FileExtension.PNG);
+    LIVE2D_MODEL_NUKKI_FILE_NAME("live2d_model_nukki", FileExtension.PNG),
+    BACKGROUND_IMAGE_FILE_NAME("background_image", extension = FileExtension.PNG),
+    THUMBNAIL_IMAGE_FILE_NAME("thumbnail_image", extension = FileExtension.PNG);
 
     override fun toString(): String = "${this.text}.${this.extension.text}"
 
