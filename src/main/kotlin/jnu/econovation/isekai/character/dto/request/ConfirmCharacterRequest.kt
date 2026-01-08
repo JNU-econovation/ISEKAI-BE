46ba7d58e0c9a4e54b8981867e092725b3dc392a
rdme0 <we6610@gmail.com>
2026-01-08 18:24:07 +0900

    :recycle: refactor: voiceId type String -> Long

--- src/main/kotlin/jnu/econovation/isekai/character/dto/request/ConfirmCharacterRequest.kt
+++ src/main/kotlin/jnu/econovation/isekai/character/dto/request/ConfirmCharacterRequest.kt
@@ -3,6 +3,6 @@ package jnu.econovation.isekai.character.dto.request
 data class ConfirmCharacterRequest(
     val uuid: String,
     val name: String,
-    val voiceId: String,
+    val voiceId: Long,
     val persona: String
 )
