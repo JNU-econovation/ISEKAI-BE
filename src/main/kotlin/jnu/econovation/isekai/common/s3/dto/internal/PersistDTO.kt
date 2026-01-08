04c3d12cc7455c9b2902c33bf4d39effbd798d70
rdme0 <we6610@gmail.com>
2026-01-08 18:25:21 +0900

    :truck: rename: 클래스 이름 더 포괄적으로

--- src/main/kotlin/jnu/econovation/isekai/common/s3/dto/internal/PersistDTO.kt
+++ src/main/kotlin/jnu/econovation/isekai/common/s3/dto/internal/PersistDTO.kt
@@ -2,7 +2,7 @@ package jnu.econovation.isekai.common.s3.dto.internal
 
 import jnu.econovation.isekai.common.s3.enums.FileName
 
-data class PersistResultDTO(
+data class PersistDTO(
     val fileName: FileName,
     val url: String
 )
