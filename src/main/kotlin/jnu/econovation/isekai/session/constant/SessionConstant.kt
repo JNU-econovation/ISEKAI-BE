5b5e37aac884c99acbb2749daa6be72d08bf76a5
rdme0 <we6610@gmail.com>
2026-01-17 17:04:33 +0900

    :truck: rename: 변수 이름 포괄적으로

--- src/main/kotlin/jnu/econovation/isekai/session/constant/SessionConstant.kt
+++ src/main/kotlin/jnu/econovation/isekai/session/constant/SessionConstant.kt
@@ -1,7 +1,7 @@
 package jnu.econovation.isekai.session.constant
 
 object SessionConstant {
-    const val FLOW_BUFFER_SIZE = 128
+    const val STREAM_BUFFER_SIZE = 128
     const val INCOMING_MESSAGE_SIZE_LIMIT = 512 * 1024
     const val OUTGOING_BUFFER_SIZE_LIMIT = 5 * 1024 * 1024
     const val SEND_TIME_LIMIT_MS = 10 * 100
